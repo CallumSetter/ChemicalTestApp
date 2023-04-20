3d042aa3543572d006c5d26e9a8c25c0eba3af88
Callum Setter <calsett@chbc.school.nz>
2023-04-20 19:24:07 +1200

    efww

--- ChemicalTestApp/Program.cs
+++ ChemicalTestApp/Program.cs
@@ -9,115 +9,140 @@ namespace ChemicalTestApp
     {
         //Global Variables
 
-        static List<string> chemicals = new List<string>(){"Cyanide", "Propane", "Alcohol"};
-        private static int chemcialTested; 
+        static List<string> chemicals = new List<string>() { "Cyanide", "Propane", "Alcohol" };
+        private static int chemcialTested;
 
-
+          
         //Methods and Functions
-
-
-
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Chemical Test App\n");
-            Console.WriteLine("---------- Menu ----------\n");
-            Console.WriteLine("Enter a Chemical for Testing\n" +
-                "For Cyanide Press 1\n" +
-                "For Propane Press 2\n" +
-                "For Alcohol Press 3\n");
-
-            // Make sure the user only enters the digits 1, 2 or 3 rather than any other number or type of input i.e. letters or symbols
-            // This will be done by creating a catch so when a number is not 
+            string ReRun = "a";
+            bool RunMe = true;
             
-            string MyInput = "0";
-            int MyInt = 0;
-
-            while ((MyInt < 1) || (MyInt > 3))
+            while (RunMe == true)
             {
-                try
+                Console.WriteLine("Welcome to Chemical Test App\n");
+                Console.WriteLine("---------- Menu ----------\n");
+                Console.WriteLine("Enter a Chemical for Testing\n" +
+                    "For Cyanide Press 1\n" +
+                    "For Propane Press 2\n" +
+                    "For Alcohol Press 3\n");
+
+                // Make sure the user only enters the digits 1, 2 or 3 rather than any other number or type of input i.e. letters or symbols
+
+
+                string MyInput = "0";
+                int MyInt = 0;
+
+                while ((MyInt < 1) || (MyInt > 3))
                 {
-                    MyInput = Console.ReadLine();
-                    MyInt = Convert.ToInt32(MyInput);
+                    try
+                    {
+                        //Get the number correlates to the chemcial
+                        MyInput = Console.ReadLine();
+                        MyInt = Convert.ToInt32(MyInput);
+                    }
+                    catch (System.FormatException)
+                    {
+                        //Display an error message letting the user know that any type of letter is not an interger
+                        Console.WriteLine("\nError: That is not an interger\n");
+                    }
+
+                    if ((MyInt < 1) || (MyInt > 3))
+                    {
+                        //Display an error message letting the user know that they must not choose a number higher than 3 or lower than 1
+                        Console.WriteLine("\nError: Please enter option 1, 2 or 3\n");
+                    }
                 }
-                catch (System.FormatException)
+
+                int chemicalTested = MyInt;
+
+
+
+
+
+
+                //Add the chemical that is being tested and measure the number of live germs
+
+
+                Random randGen = new Random();
+
+                float sumEfficency = 0;
+
+
+
+
+                for (int i = 0; i < 5; i++)
                 {
-                    Console.WriteLine("\nThat is not an interger\n");
+                    int initailChemicalTest = randGen.Next(1000, 100000);
+
+                    Console.WriteLine($"Initail Germ Count: {initailChemicalTest}");
+
+                    //After an amount of time the number of live germs is again measured
+
+                    Console.WriteLine("Chemical is Being Tested Please Wait 5 Seconds\n");
+
+
+                    System.Threading.Thread.Sleep(1000);
+
+
+                    //Live germs are measured again
+                    int afterChemicalTest = randGen.Next(0, initailChemicalTest);
+
+                    Console.WriteLine($"Number of Germs Left: {afterChemicalTest}\n");
+
+                    //Determine the efficiency of the chemical of it killing germs
+
+                    float efficiency = (float)(initailChemicalTest - afterChemicalTest) / 5;
+
+                    sumEfficency += efficiency;
+
+
+                    //Loop 5 times
                 }
 
-                if ((MyInt < 1) || (MyInt > 3))
-                {
-                    Console.WriteLine("\nPlease enter option 1, 2 or 3\n");
-                }
-            }
 
-            int chemicalTested = MyInt;
+                //Calculate the final effciency
+
+                float finalEffciency = sumEfficency / 5;
 
 
 
+                //Display the chemical name and its final effciency rating
 
+                Console.WriteLine($"Chemical tested: {chemicals[chemicalTested - 1]} {finalEffciency} \n");
 
-
-            //Add the chemical that is being tested and measure the number of live germs
-            
-
-            Random randGen = new Random();
-
-            float sumEfficency = 0;
-
-            
-
-
-            for (int i = 0; i < 5; i++)
-            {
-                int initailChemicalTest = randGen.Next(1000, 100000);
-
-                Console.WriteLine($"Initail Germ Count: {initailChemicalTest}");
-
-                //After an amount of time the number of live germs is again measured
-
-                Console.WriteLine("Chemical is Being Tested Please Wait 5 Seconds\n");
-
-               
-                System.Threading.Thread.Sleep(1000); 
+                //Ask the user if they want to test another chemical or do they want to stop
                 
 
-                //Live germs are measured again
-                int afterChemicalTest = randGen.Next(0, initailChemicalTest);
+                do
+                {
 
-                Console.WriteLine($"Number of Germs Left: {afterChemicalTest}\n");
+                    Console.WriteLine("Thank you for using ChemicalTestApp\n");
+                    Console.WriteLine("To rerun press y or to quit press n\n");
 
-                //Determine the efficiency of the chemical of it killing germs
+                    ReRun = Console.ReadLine();
 
-                float efficiency = (float)(initailChemicalTest - afterChemicalTest) / 5;
+                    if (ReRun == "y")
+                    {
+                        RunMe = true;
+                    }
+                    else if (ReRun == "n")
+                    {
+                        RunMe = false;
+                    }
+                    else { Console.WriteLine("Please enter either y or n\n\n"); }
+                }
+                while ((ReRun != "y") || (ReRun != "n"));
 
-                sumEfficency += efficiency;
+                //The process gets repeated with other chemicals
 
-
-                //Loop 5 times
+                    //Record the data and determine the least and most efficient chemical
             }
 
-
-            //Calculate the final effciency
-
-            float finalEffciency = sumEfficency / 5;
-
-             
-
-            //Display the chemical name and its final effciency rating
-
-            Console.WriteLine($"Chemical tested: {chemicals[chemicalTested -1]} {finalEffciency} \n");
-
-           
-
-
-            //The process gets repeated with other chemicals
-
-            //Record the data and determine the least and most efficient chemical
+            //
         }
-
-        //
-
 
     }
 }
